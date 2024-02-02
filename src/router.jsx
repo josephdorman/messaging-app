109b4625f965ddab12a3906def53e58ace1b2278
Joseph Dorman <JDorman555@yahoo.com>
2024-02-02 00:57:31 -0600

    feat: add route to signup page

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import Signup from "./components/signup";
 
 function Router() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
+    },
+    {
+      path: "/",
+      element: <Signup />,
     },
   ]);
 
