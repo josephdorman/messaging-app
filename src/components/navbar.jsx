e6b2ff36fe27898df93d9f6a0dcdc36ee63b98ff
Joseph Dorman <JDorman555@yahoo.com>
2024-02-03 21:16:34 -0600

    feat: create navbar ui

---
+++ src/components/navbar.jsx
@@ -0,0 +1,32 @@
+import "../styles/navbar.css";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="line">
+        <img className="logo-sm" src={logo} alt="" />
+      </div>
+      <nav>
+        <button id="home" className="nav-btn">
+          <Link to="/home"></Link>
+        </button>
+        <button id="notifs" className="nav-btn">
+          <Link to="/notifs"></Link>
+        </button>
+        <button id="friends" className="nav-btn">
+          <Link to="/friends"></Link>
+        </button>
+        <button id="messages" className="nav-btn">
+          <Link to="/messages"></Link>
+        </button>
+      </nav>
+      <button id="logout" className="nav-btn">
+        <Link to="/logout"></Link>
+      </button>
+    </div>
+  );
+}
+
+export default Navbar;
