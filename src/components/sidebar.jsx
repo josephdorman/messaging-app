211725e1b3ee5eef7cf82811d9226665c62554e1
Joseph Dorman <JDorman555@yahoo.com>
2024-02-08 23:30:14 -0600

    style: fill with mock data and style scroll/fix columns

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -3,62 +3,110 @@ import profile from "../assets/profileIcon.svg";
 import chat from "../assets/chatLinear.svg";
 
 function Sidebar() {
+  function fillChat() {
+    return (
+      <>
+        <div className="ch-wrapper">
+          <div className="ch">
+            <img className="icon-md" src={profile} alt="" />
+            <div>
+              <h3 className="ch-name">Philip Morano</h3>
+              <p className="last-msg">Are you busy atm?</p>
+            </div>
+          </div>
+          <div className="ch-info">
+            <p>10:22</p>
+            <p>(2)</p>
+          </div>
+        </div>
+        <div className="ch-wrapper">
+          <div className="ch">
+            <img className="icon-md" src={profile} alt="" />
+            <div>
+              <h3 className="ch-name">Philip Morano</h3>
+              <p className="last-msg">Are you busy atm?</p>
+            </div>
+          </div>
+          <div className="ch-info">
+            <p>10:22</p>
+            <p>(2)</p>
+          </div>
+        </div>
+        <div className="ch-wrapper">
+          <div className="ch">
+            <img className="icon-md" src={profile} alt="" />
+            <div>
+              <h3 className="ch-name">Philip Morano</h3>
+              <p className="last-msg">Are you busy atm?</p>
+            </div>
+          </div>
+          <div className="ch-info">
+            <p>10:22</p>
+            <p>(2)</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <div className="sidebar">
       <div className="category">
         <h2>Messages</h2>
         <p>People, Group, Messages</p>
       </div>
-      <div className="ch-list-wrapper">
-        <div className="search">
-          <button id="search" className="nav-btn"></button>
-          <input
-            className="search-bar"
-            type="text"
-            placeholder="Search People, Groups, Messages"
-          />
-        </div>
-        <div className="list">
-          <div className="ch-wrapper">
-            <div className="ch">
-              <img className="icon-md" src={profile} alt="" />
-              <div>
-                <h3 className="ch-name">Philip Morano</h3>
-                <p className="last-msg">Are you busy atm?</p>
-              </div>
-            </div>
-            <div className="ch-info">
-              <p>10:22</p>
-              <p>(2)</p>
+      <div className="search">
+        <button id="search" className="nav-btn"></button>
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search People, Groups, Messages"
+        />
+      </div>
+      <div className="list">
+        <div className="ch-wrapper">
+          <div className="ch">
+            <img className="icon-md" src={profile} alt="" />
+            <div>
+              <h3 className="ch-name">Philip Morano</h3>
+              <p className="last-msg">Are you busy atm?</p>
             </div>
           </div>
-          <div className="ch-wrapper">
-            <div className="ch">
-              <img className="icon-md" src={profile} alt="" />
-              <div>
-                <h3 className="ch-name">Philip Morano</h3>
-                <p className="last-msg">Are you busy atm?</p>
-              </div>
-            </div>
-            <div className="ch-info">
-              <p>10:22</p>
-              <p>(2)</p>
-            </div>
-          </div>
-          <div className="ch-wrapper">
-            <div className="ch">
-              <img className="icon-md" src={profile} alt="" />
-              <div>
-                <h3 className="ch-name">Philip Morano</h3>
-                <p className="last-msg">Are you busy atm?</p>
-              </div>
-            </div>
-            <div className="ch-info">
-              <p>10:22</p>
-              <p>(2)</p>
-            </div>
+          <div className="ch-info">
+            <p>10:22</p>
+            <p>(2)</p>
           </div>
         </div>
+        <div className="ch-wrapper">
+          <div className="ch">
+            <img className="icon-md" src={profile} alt="" />
+            <div>
+              <h3 className="ch-name">Philip Morano</h3>
+              <p className="last-msg">Are you busy atm?</p>
+            </div>
+          </div>
+          <div className="ch-info">
+            <p>10:22</p>
+            <p>(2)</p>
+          </div>
+        </div>
+        <div className="ch-wrapper">
+          <div className="ch">
+            <img className="icon-md" src={profile} alt="" />
+            <div>
+              <h3 className="ch-name">Philip Morano</h3>
+              <p className="last-msg">Are you busy atm?</p>
+            </div>
+          </div>
+          <div className="ch-info">
+            <p>10:22</p>
+            <p>(2)</p>
+          </div>
+        </div>
+        {fillChat()}
+        {fillChat()}
+        {fillChat()}
+        {fillChat()}
       </div>
       <div className="ch user">
         <img className="icon-md" src={profile} alt="" />
