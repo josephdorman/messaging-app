e7a34a1feff22256bfa4b1ba1372502460a086a6
Joseph Dorman <JDorman555@yahoo.com>
2024-02-08 23:06:19 -0600

    chore: add mock data

--- src/components/channelView.jsx
+++ src/components/channelView.jsx
@@ -2,6 +2,41 @@ import "../styles/channelView.css";
 import profile from "../assets/profileIcon.svg";
 
 function ChannelView() {
+  function fillChat() {
+    return (
+      <>
+        <div className="msg-wrapper">
+          <img className="icon-md" src={profile} alt="" />
+          <div className="msg">
+            <div className="msg-user">
+              <h4>Katie Lundon</h4>
+              <p>10:22 PM</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+        <div className="msg-wrapper">
+          <img className="icon-md" src={profile} alt="" />
+          <div className="msg">
+            <div className="msg-user">
+              <h4>Philip Morano</h4>
+              <p>7:45 PM</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor.
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <div className="ch-view">
       <div className="ch ch-header">
@@ -29,6 +64,26 @@ function ChannelView() {
             </p>
           </div>
         </div>
+        <div className="msg-wrapper">
+          <img className="icon-md" src={profile} alt="" />
+          <div className="msg">
+            <div className="msg-user">
+              <h4>Philip Morano</h4>
+              <p>7:45 PM</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor.
+            </p>
+          </div>
+        </div>
+        {fillChat()}
+        {fillChat()}
+        {fillChat()}
+        {fillChat()}
+        {fillChat()}
+        {fillChat()}
+        {fillChat()}
       </div>
       <div className="ch-actions">
         <input
