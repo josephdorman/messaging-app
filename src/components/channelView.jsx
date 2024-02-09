8a1673a3aba705034adda6f489a33975bd7abc6f
Joseph Dorman <JDorman555@yahoo.com>
2024-02-08 22:27:01 -0600

    feat: create ui for channel/chat view

---
+++ src/components/channelView.jsx
@@ -0,0 +1,46 @@
+import "../styles/channelView.css";
+import profile from "../assets/profileIcon.svg";
+
+function ChannelView() {
+  return (
+    <div className="ch-view">
+      <div className="ch ch-header">
+        <img className="icon-lg" src={profile} alt="" />
+        <div>
+          <h3>Product Team</h3>
+          <p>
+            <span>6 Members</span>, <span>2 Online</span>
+          </p>
+        </div>
+      </div>
+      <div className="ch-chat">
+        <div className="msg-wrapper">
+          <img className="icon-md" src={profile} alt="" />
+          <div className="msg">
+            <div className="msg-user">
+              <h4>Katie Lundon</h4>
+              <p>10:22</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="ch-actions">
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Type message here..."
+        />
+        <button id="emoji" className="nav-btn"></button>
+        <button id="attach" className="nav-btn"></button>
+      </div>
+    </div>
+  );
+}
+
+export default ChannelView;
