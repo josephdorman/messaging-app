80341b404ea05b3dc3b64c2a443b888b57231bf6
Joseph Dorman <JDorman555@yahoo.com>
2024-02-18 22:03:22 -0600

    feat: redirect user to login if no session exists

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import "./styles/app.css";
 import Navbar from "./components/navbar";
 import Sidebar from "./components/sidebar";
 import ChannelView from "./components/channelView";
+import { Navigate } from "react-router-dom";
+import { getSession } from "./providers/api";
+import { useEffect } from "react";
 
-function App() {
+function App({ user, setUser }) {
+  useEffect(() => {
+    getSession().then((res) => {
+      setUser(res);
+    });
+  });
+
   return (
-    <div className="app">
-      <Navbar />
-      <Sidebar />
-      <ChannelView />
-    </div>
+    <>
+      {user ? (
+        <div className="app">
+          <Navbar />
+          <Sidebar />
+          <ChannelView />
+        </div>
+      ) : (
+        <Navigate to="/login" replace={true} />
+      )}
+    </>
   );
 }
 
