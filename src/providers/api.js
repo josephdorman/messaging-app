0cb10d8d6dd580ee7a420b7110e0908b3aba5987
Joseph Dorman <JDorman555@yahoo.com>
2024-02-18 21:56:38 -0600

    feat: create function for login api call

--- src/providers/api.js
+++ src/providers/api.js
@@ -16,4 +16,20 @@ function createUser(e) {
     });
 }
 
-export { createUser };
+async function loginUser(e) {
+  e.preventDefault();
+  axios.defaults.withCredentials = true;
+  return axios
+    .post("http://localhost:3000/api/v1/user/login", {
+      username: e.target.form.name.value,
+      password: e.target.form.password.value,
+    })
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+export { createUser, loginUser };
