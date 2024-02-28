4fde9f7f1c1781f2dda2cb6fb5bfa7d70c65680a
Joseph Dorman <JDorman555@yahoo.com>
2024-02-27 20:58:56 -0600

    feat: finish settings sidebar ui

---
+++ src/pages/Settings/sidebar.jsx
@@ -0,0 +1,38 @@
+import "../../styles/settings.css";
+import SidebarComp from "../../components/sidebar";
+import profile from "../../assets/profileIcon.svg";
+
+function Sidebar() {
+  return (
+    <>
+      <SidebarComp
+        searchbar={false}
+        body={
+          <>
+            <div className="category">
+              <h2>Settings</h2>
+              <p>Manage accounts, profile, and more</p>
+            </div>
+            <div className="list">
+              <div className="setting-category">
+                <h2>User Settings</h2>
+                <button className="setting">My Account</button>
+                <button className="setting">Profiles</button>
+                <button className="setting">Security</button>
+              </div>
+              <div className="setting-category">
+                <h2>App Settings</h2>
+                <button className="setting">Theme</button>
+                <button className="setting">Language</button>
+                <button className="setting">Display</button>
+                <button className="setting">Help</button>
+              </div>
+            </div>
+          </>
+        }
+      />
+    </>
+  );
+}
+
+export default Sidebar;
