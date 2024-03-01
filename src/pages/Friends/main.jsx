3cdf6b92723f82f11f3902cce36ecf7480ae53e8
Joseph Dorman <JDorman555@yahoo.com>
2024-02-29 22:11:45 -0600

    feat: add nav bar and icons

--- src/pages/Friends/main.jsx
+++ src/pages/Friends/main.jsx
@@ -1,4 +1,5 @@
 import profile from "../../assets/profileIcon.svg";
+import addImg from "../../assets/addLinear.svg";
 import MainComp from "../../components/main";
 
 function Main() {
@@ -7,19 +8,29 @@ function Main() {
       <MainComp
         body={
           <>
-            <img className="icon-xl" src={profile} alt=""></img>
-            <h3 className="friend-pfn">Paul Morano</h3>
-            <nav className="nav-profile">
-              <button id="home" className="nav-btn"></button>
-              <button id="home" className="nav-btn"></button>
-              <button id="home" className="nav-btn"></button>
+            <nav className="nav-friends">
+              <button className="chunky">Online</button>
+              <button className="chunky">Blocked</button>
+              <button id="add-friend" className="chunky">
+                Add Friend
+                <img src={addImg} className="nav-btn" alt="" />
+              </button>
             </nav>
-            <div className="about-me">
-              <p className="chunky">About Me</p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
+            <div className="pf">
+              <img className="icon-xl" src={profile} alt=""></img>
+              <h3 className="friend-pfn">Paul Morano</h3>
+              <nav className="nav-profile">
+                <button id="messages" className="nav-btn"></button>
+                <button id="remove" className="nav-btn"></button>
+                <button id="warn" className="nav-btn"></button>
+              </nav>
+              <div className="about-me">
+                <p className="chunky">About Me</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+              </div>
             </div>
           </>
         }
