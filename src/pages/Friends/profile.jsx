4c04ef7ff10c2ff13a9053121a865e340c2a9661
Joseph Dorman <JDorman555@yahoo.com>
2024-03-02 19:39:58 -0600

    feat: display friend profiles

--- src/pages/Friends/profile.jsx
+++ src/pages/Friends/profile.jsx
@@ -1,24 +1,50 @@
 import profile from "../../assets/profileIcon.svg";
+import { useEffect, useState } from "react";
+import { useMatch } from "react-router-dom";
+import { getFriendProfile } from "../../providers/api";
 
 function Profile() {
+  const [friendProfile, setFriendProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const match = useMatch("/friends/:id");
+
+  useEffect(() => {
+    getFriendProfile(match.params.id).then((res) => {
+      setFriendProfile(res);
+
+      if (res !== false) {
+        setIsLoading(false);
+      } else {
+        setIsError(true);
+      }
+    });
+  }, [match]);
+
   return (
     <>
-      <div className="pf">
-        <img className="icon-xl" src={profile} alt=""></img>
-        <h3 className="friend-pfn">Paul Morano</h3>
-        <nav className="nav-profile">
-          <button id="messages" className="nav-btn"></button>
-          <button id="remove" className="nav-btn"></button>
-          <button id="warn" className="nav-btn"></button>
-        </nav>
-        <div className="about-me">
-          <p className="chunky">About Me</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+      {isError ? (
+        <p>User does not exist or is not a friend</p>
+      ) : isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="pf">
+          <img className="icon-xl" src={profile} alt=""></img>
+          <h3 className="friend-pfn">{friendProfile.username}</h3>
+          <nav className="nav-profile">
+            <button id="messages" className="nav-btn"></button>
+            <button id="remove" className="nav-btn"></button>
+            <button id="warn" className="nav-btn"></button>
+          </nav>
+          <div className="about-me">
+            <p className="chunky">About Me</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
