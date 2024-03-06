ea373d37bd7c13f133dd913fe35768b62146ac92
Joseph Dorman <JDorman555@yahoo.com>
2024-03-05 23:12:50 -0600

    feat: display users status in real time

--- src/pages/Friends/online.jsx
+++ src/pages/Friends/online.jsx
@@ -1,7 +1,29 @@
+import profile from "../../assets/profileIcon.svg";
+import { useState, useEffect, useContext } from "react";
+import SocketContext from "../../providers/socketContext";
+
 function Online() {
+  const { socket } = useContext(SocketContext);
+  const [friends, setFriends] = useState(null);
+
+  useEffect(() => {
+    socket.emit("get_friends");
+    socket.on("receive_friends", (data) => {
+      setFriends(data);
+    });
+  }, [socket]);
+
   return (
     <>
-      <div>THIS IS ONLINE VIEW</div>
+      <div className="friends-online">
+        {friends &&
+          friends.map((friend) => (
+            <div key={friend.socketId} className="fr-online">
+              <img className="icon-md" src={profile} alt="" />
+              <p>{friend.userId}</p>
+            </div>
+          ))}
+      </div>
     </>
   );
 }
