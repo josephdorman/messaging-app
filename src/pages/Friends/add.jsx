81d2893d631c3bb6c35800b1ddbc8f4cb3a19f25
Joseph Dorman <JDorman555@yahoo.com>
2024-03-05 23:40:01 -0600

    feat: finish friend adding ui

--- src/pages/Friends/add.jsx
+++ src/pages/Friends/add.jsx
@@ -1,7 +1,18 @@
 function Add() {
   return (
     <>
-      <div>test</div>
+      <div className="add-friend-wrapper">
+        <h3 className="chunky">Add Friend</h3>
+        <p>You can add friends here by their username</p>
+        <div className="search friend-add">
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="You can add friends here by their username"
+          />
+          <button>Send Friend Request</button>
+        </div>
+      </div>
     </>
   );
 }
