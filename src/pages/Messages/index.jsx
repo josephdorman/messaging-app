a91d67f76e90f80c51c8b5da8dee8fcb78a1e6d4
Joseph Dorman <JDorman555@yahoo.com>
2024-03-20 23:05:32 -0500

    chore: remove useless code

--- src/pages/Messages/index.jsx
+++ src/pages/Messages/index.jsx
@@ -1,20 +1,7 @@
 import "../../styles/messages.css";
-import profile from "../../assets/profileIcon.svg";
-import UserContext from "../../providers/userContext";
-import { getFriends } from "../../providers/api";
 import Sidebar from "./sidebar";
-import { useState, useEffect, useContext } from "react";
 
 function Index() {
-  /*
-  const { user, setUser } = useContext(UserContext);
-  const [friends, setFriends] = useState(null);
-
-  useEffect(() => {
-    getFriends();
-  }, []);
-  */
-
   return (
     <>
       <Sidebar />
