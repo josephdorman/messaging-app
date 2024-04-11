5c73016549733e3f0606e848def7852dd2c0a247
Joseph Dorman <JDorman555@yahoo.com>
2024-04-10 21:29:59 -0500

    feat: ui for channel management'

--- src/pages/Messages/main.jsx
+++ src/pages/Messages/main.jsx
@@ -6,6 +6,7 @@ import { useMatch } from "react-router-dom";
 import SocketContext from "../../providers/socketContext";
 import UserContext from "../../providers/userContext";
 import useDateFormat from "../../hooks/useDateFormat";
+import deleteIcon from "../../assets/deleteLinear.svg";
 
 function Main({ setCurrentChannel, currentChannel }) {
   const [isLoading, setIsLoading] = useState(true);
@@ -86,25 +87,48 @@ function Main({ setCurrentChannel, currentChannel }) {
                       everybody
                     </p>
                   </div>
-                  {channel.owner && <button id="manage">Manage</button>}
-                </div>
-                <div className="ch-chat" ref={chatRef} onLoad={resetScroll}>
-                  {channel.messages.length > 0 ? (
-                    channel.messages.map((msg) => (
-                      <div key={msg._id} className="msg-wrapper">
-                        <img className="icon-md" src={profile} alt="" />
-                        <div className="msg-user">
-                          <h4>{msg.user.username}</h4>
-                          <p className="chunky">{useDateFormat(msg.date)}</p>
-                        </div>
-                        <p className="msg-body">{msg.body}</p>
-                      </div>
-                    ))
-                  ) : (
-                    <div className="chunky">
-                      No messages yet, type one to begin a chat
-                    </div>
+                  {channel.owner && (
+                    <button className="fr-nav" id="manage">
+                      Manage Channel
+                    </button>
                   )}
+                </div>
+                <div className="channel-view">
+                  <div className="ch-chat" ref={chatRef} onLoad={resetScroll}>
+                    {channel.messages.length > 0 ? (
+                      channel.messages.map((msg) => (
+                        <div key={msg._id} className="msg-wrapper">
+                          <img className="icon-md" src={profile} alt="" />
+                          <div className="msg-user">
+                            <h4>{msg.user.username}</h4>
+                            <p className="chunky">{useDateFormat(msg.date)}</p>
+                          </div>
+                          <p className="msg-body">{msg.body}</p>
+                        </div>
+                      ))
+                    ) : (
+                      <div className="chunky">
+                        No messages yet, type one to begin a chat
+                      </div>
+                    )}
+                  </div>
+                  <div className="ch-chat-sidebar">
+                    <button id="delete-channel" className="ch-chat-sb-btn">
+                      <div id="delete-ch" className="nav-btn" alt=""></div>
+                      Delete Channel
+                    </button>
+                    <p className="chunky">Mange users</p>
+                    <p className="ch-chat-sb-title">Add or kick users here</p>
+                    <form className="search friend-add ch-chat-sb">
+                      <input
+                        className="search-bar ch-chat-sb"
+                        type="text"
+                        id="sendReq"
+                        name="sendReq"
+                        placeholder="Search for user here"
+                      ></input>
+                    </form>
+                  </div>
                 </div>
                 <div className="send-msg-wrapper">
                   <input
