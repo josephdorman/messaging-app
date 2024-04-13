c4597502d3129789261e4be064c3b4d3f36953f6
Joseph Dorman <JDorman555@yahoo.com>
2024-04-12 20:33:46 -0500

    feat: add ui for searched users

--- src/pages/Messages/main.jsx
+++ src/pages/Messages/main.jsx
@@ -135,6 +135,18 @@ function Main({ setCurrentChannel, currentChannel }) {
                         placeholder="Search for user here"
                       ></input>
                     </form>
+                    <div className="list">
+                      <div className="def-btn ch-wrapper">
+                        <img className="icon-md ch-icon" src={profile} alt="" />
+                        <h3 className="ch-name ch-name-fr">Paul Morano</h3>
+                        <button id="add-user-ch">Add</button>
+                      </div>
+                      <div className="def-btn ch-wrapper">
+                        <img className="icon-md ch-icon" src={profile} alt="" />
+                        <h3 className="ch-name ch-name-fr">Kate Bennet</h3>
+                        <button id="remove-user-ch">Remove</button>
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div className="send-msg-wrapper">
