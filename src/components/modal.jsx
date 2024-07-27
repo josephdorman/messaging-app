d0388dcd953ab230b5abc8e2d1d9ce3fdfa71c2e
Joseph Dorman <JDorman555@yahoo.com>
2024-07-26 19:20:44 -0500

    feat: make component more flexible

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -4,8 +4,10 @@ function Modal(props) {
   return (
     <div className="modal">
       <h3>{props.title}</h3>
-      {props.perm && <p className="chunky">This action can not be reversed</p>}
-      {props.btns}
+      {props.perm && (
+        <p className="chunky modal-warning">This action can not be reversed</p>
+      )}
+      <div className="modal-btns">{props.btns}</div>
     </div>
   );
 }
